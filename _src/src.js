22824a64f1574fc743eefb1908a7ed26131ac13b
Aaron Ervin <aervin@uste3.com>
2017-09-18 18:37:29 -0400

    init implementation

---
+++ _src/src.js
@@ -0,0 +1,89 @@
+function DatekeyHelper(datekey, config) {
+    var _this = this
+    _this.datekey = datekey || undefined
+    _this.config = config || undefined
+
+    if (
+        datekeyExists(_this.datekey) &&
+        validateType(_this.datekey) &&
+        validateLength(_this.datekey)
+    ) {
+        return _this
+    }
+
+    _this.display = {
+        long: today(datekey, true),
+        short: today(datekey, false)
+    }
+
+    _this.date = new Date(_this.display.long)
+    _this.isSaturday = _this.date.getDay() === 6
+    _this.isSunday = _this.date.getDay() === 0
+
+    _this.weekEndings = function(n) {
+        var endings = []
+        if (n === 0 || n === undefined) {
+            return endings
+        }
+        var start = new Date(_this.display.long)
+        if (!_this.isSaturday) {
+            if (n > 0) {
+                var offset = 6 - _this.date.getDay()
+                start.setDate(start.getDate() + offset)
+            } else {
+                start.setDate(start.getDate() - (_this.date.getDay() + 1))
+            }
+        }
+        for (var i = 0; i < Math.abs(n); i++) {
+            endings.push(dateToDatekey(start))
+            n > 0
+                ? start.setDate(start.getDate() + 7)
+                : start.setDate(start.getDate() - 7)
+        }
+        return endings
+    }
+}
+
+function datekeyExists(d) {
+    if (d === undefined || d === null) {
+        throw new Error(`
+            Cannot instantiate DatekeyHelper. Was not passed a datekey.
+        `)
+    }
+}
+function dateToDatekey(d) {
+    var y = d.getFullYear()
+    var m =
+        d.getMonth() + 1 > 9
+            ? d.getMonth() + 1
+            : `0` + (d.getMonth() + 1).toString()
+    var d = d.getDate() > 9 ? d.getDate() : `0` + d.getDate().toString()
+    return parseInt(`${y}${m}${d}`)
+}
+function today(datekey, long) {
+    var a = datekey.toString().split('')
+    var y = `${a[0]}${a[1]}${a[2]}${a[3]}`
+    var m = `${a[4]}${a[5]}`
+    var d = `${a[6]}${a[7]}`
+    var s = m + '/' + d
+    var l = m + '/' + d + '/' + y
+    return long ? l : s
+}
+function validateLength(d) {
+    if (d.toString().length !== 8) {
+        throw new Error(`
+            Cannot instantiate DatekeyHelper. Was passed a
+            datekey which is not the right length.
+        `)
+    }
+}
+function validateType(d) {
+    if (typeof d !== 'number') {
+        throw new Error(`
+            Cannot instantiate DatekeyHelper. Was passed a
+            datekey which is not a number.
+        `)
+    }
+}
+
+module.exports = DatekeyHelper
---
+++ _test/run.js
@@ -0,0 +1,63 @@
+const assert = require('assert')
+const DatekeyHelper = require('./../datekey-helper')
+
+const logPassFail = (passed, testCount, actual, expected) => {
+    console.log(`
+    [${testCount}] - ${passed ? 'PASS' : 'FAIL'}!
+        It should return:
+            ${expected}
+        Got:
+            ${actual}
+    `)
+}
+
+const datekey = new DatekeyHelper(20170917)
+let testCount, expected, actual
+
+// [1]
+testCount = 1
+expected = '09/17/2017'
+actual = datekey.display.long
+try {
+    assert.strictEqual(actual, expected)
+    logPassFail(true, testCount, actual, expected)
+} catch (e) {
+    logPassFail(false, testCount, actual, expected)
+    process.exit(1)
+}
+
+// [2]
+testCount++
+expected = '09/17'
+actual = datekey.display.short
+try {
+    assert.strictEqual(actual, expected)
+    logPassFail(true, testCount, actual, expected)
+} catch (e) {
+    logPassFail(false, testCount, actual, expected)
+    process.exit(1)
+}
+
+// [3]
+testCount++
+expected = [20170923, 20170930, 20171007, 20171014]
+actual = datekey.weekEndings(4)
+try {
+    assert.deepEqual(actual, expected)
+    logPassFail(true, testCount, actual, expected)
+} catch (e) {
+    logPassFail(false, testCount, actual, expected)
+    process.exit(1)
+}
+
+// [4]
+testCount++
+expected = [20170916, 20170909, 20170902, 20170826]
+actual = datekey.weekEndings(-4)
+try {
+    assert.deepEqual(actual, expected)
+    logPassFail(true, testCount, actual, expected)
+} catch (e) {
+    logPassFail(false, testCount, actual, expected)
+    process.exit(1)
+}
